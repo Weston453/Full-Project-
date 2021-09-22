e4af9533557a100196f695952bbbeb2806de6d5d
Weston Isheim <wisheim@gmail.com>
2021-09-21 20:46:23 -0600

    Basic HTML and CSS completed

---
+++ Client/index.js
@@ -0,0 +1,7 @@
+let addCard = (event) => {
+    // event.preventDefault()
+    const radBtn = document.getElementById('radBtn')
+    radBtn.addEventListener('click', console.log("I got clicked"))
+
+}
+
