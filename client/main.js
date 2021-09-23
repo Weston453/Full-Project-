479b4133600e942f1f4a2fd2088944f4023674d8
Weston Isheim <wisheim@gmail.com>
2021-09-23 17:02:25 -0600

    centered my card

--- client/main.js
+++ client/main.js
@@ -1,7 +1,12 @@
+let radBtn = document.getElementById('radBtn')
+
+
 let addCard = (event) => {
-    // event.preventDefault()
-    const radBtn = document.getElementById('radBtn')
-    radBtn.addEventListener('click', console.log("I got clicked"))
+    event.preventDefault()
+    // radBtn.addEventListener('click', console.log("i got clicked"))
+    alert("blah blah blah")
+};
 
-}
-
+// showVideo = () =>{
+//     document.createElement(section)
+// };
